be05272f37ed001992682966664163984137d53e
nguyenhuudungz <nguyenhuudungz@yahoo.com>
2020-11-28 23:26:20 +0700

    build: to heroku

--- dist/app.service.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class AppService {
-    getHello(): string;
-}
-export declare class TodoListService {
-    getTodoList(): any;
-    getItemById(id: any): any;
-    createNewItem(title: any): void;
-    deleteItemById(id: any): void;
-}
