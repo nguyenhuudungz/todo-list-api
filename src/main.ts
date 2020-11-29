f06aed782a3ece5f66013b7578eafb69351798ab
nguyenhuudungz <nguyenhuudungz@yahoo.com>
2020-11-29 09:21:39 +0700

    build: to heroku

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { NestFactory } from '@nestjs/core';
+import * as fs from 'fs';
+import * as path from 'path';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,6 +10,11 @@ async function bootstrap() {
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
+  });
+  fs.writeFile(path.resolve(__dirname, 'data.json'), '[]', { flag: 'wx' }, function (err) {
+    if (err) {
+      console.log('Data.json is existed.');
+    }
   });
   await app.listen(process.env.PORT || 8000);
 }
